73d00827bdca6ed1e105c26c8b25f4f1e7e09c95
lde-mich <luca01112003@gmail.com>
2024-03-13 18:06:19 +0100

    upload-mar-13

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,12 +6,11 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 12:53:13 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/03/13 17:08:47 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/03/13 18:02:53 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
-
 
 BitcoinExchange::BitcoinExchange()
 {
@@ -20,7 +19,7 @@ BitcoinExchange::BitcoinExchange()
 
 BitcoinExchange::BitcoinExchange(BitcoinExchange const &obj)
 {
-
+	(*this) = obj;
 }
 
 BitcoinExchange::~BitcoinExchange()
@@ -31,8 +30,22 @@ BitcoinExchange::~BitcoinExchange()
 
 BitcoinExchange BitcoinExchange::operator=(BitcoinExchange const &obj)
 {
+	if (this == &obj)
+		return (*this);
 
+	this->database = obj.database;
+	this->input = obj.input;
+
+	return (*this);
 }
+
+
+
+
+
+
+
+
 
 
 
