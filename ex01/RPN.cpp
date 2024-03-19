e187e590870c3216c66ba45cd24237fa5a90329d
lde-mich <luca01112003@gmail.com>
2024-03-19 11:54:59 +0100

    upload-mar-19

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,11 +6,51 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 17:29:30 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/03/18 17:29:51 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/03/19 11:54:25 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
+
+
+RPN::RPN()
+{
+
+}
+
+RPN::RPN(RPN const &obj)
+{
+
+}
+
+RPN::~RPN()
+{
+
+}
+
+
+RPN &RPN::operator=(RPN const &obj)
+{
+	if (this = &obj)
+		return (*this);
+
+	this->stack = obj.stack;
+
+	return (*this);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 17:28:34 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/03/18 17:39:30 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/03/19 11:46:06 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,10 @@ class RPN
 
 	public:
 
+		RPN &operator=(RPN const &obj);
 
+		RPN();
+		RPN(RPN const &obj);
+		~RPN();
 
 };
